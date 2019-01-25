fef668b4205ad1224cdf93ecbe345da5653f7315
shadowmimosa <chexuandong@foxmail.com>
2019-01-25 12:12:42 +0800

    后台提交到新的分支，避免每次提交冲突及冗余

--- robot_on_py3/Public/OtherMethod/thriftClient/header.thrift
+++
@@ -1,7 +0,0 @@
-namespace java com.alibaba.dubbo.rpc.protocol.kgthrift.header
-
-struct HeaderContent{
-   1:required i64 seqId,
-   2:required string servicename,
-   3:optional map<string,string> attachment,
-}
--- robot_on_py3/Public/OtherMethod/thriftClient/test.thrift
+++
@@ -1,4 +0,0 @@
-namespace py com.kugou.base.globalid.service
-service GlobalIdService{
-	   i64 get(1:i64 Operator = 0),
-}
